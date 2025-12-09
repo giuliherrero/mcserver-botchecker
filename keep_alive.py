a33a9bf668cdc1646643f4a548df1c02ea539b11
giuliherrero <giulianoherrero@gmail.com>
2025-12-08 21:25:54 -0300

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,20 @@
+import os # <-- ¡Asegúrate de incluir 'os' aquí!
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hola, estoy vivo!"
+
+# Función que inicia el servidor de Flask
+def run():
+    # Render usa la variable de entorno PORT (que es 10000)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host='0.0.0.0', port=port)
+
+# Función que llama el bot principal para que el servidor corra en segundo plano
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
